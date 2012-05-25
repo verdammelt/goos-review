331666173270c4847e7b0dae876f762218b6c490
Mark Simpson <verdammelt@gmail.com>
2012-05-24 23:16:23 -0400

    Set up the code from Chapter 10

---
+++ goos-java/src/test/java/ApplicationRunner.java
@@ -0,0 +1,14 @@
+public class ApplicationRunner {
+    public void startBiddingIn(FakeAuctionServer auction) {
+        throw new NoSuchMethodError();
+    }
+
+    public void showsSniperHasLostAuction() {
+        throw new NoSuchMethodError();
+    }
+
+    public void stop() {
+        throw new NoSuchMethodError();
+
+    }
+}
---
+++ goos-java/src/test/java/FakeAuctionServer.java
@@ -0,0 +1,20 @@
+public class FakeAuctionServer {
+    public FakeAuctionServer(String item) {
+    }
+
+    public void startSellingItem() {
+        throw new NoSuchMethodError();
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+        throw new NoSuchMethodError();
+    }
+
+    public void announceClosed() {
+        throw new NoSuchMethodError();
+    }
+
+    public void stop() {
+        throw new NoSuchMethodError();
+    }
+}
