685970ba7fb406c13638a17bd499fef140951d7f
Mark Simpson <verdammelt@gmail.com>
2012-11-17 15:53:40 -0500

    chapter 15: changed code to have a table.

---
+++ goos-java/src/main/java/auctionsniper/SnipersTableModel.java
@@ -0,0 +1,28 @@
+package auctionsniper;
+
+import javax.swing.table.AbstractTableModel;
+
+import static auctionsniper.MainWindow.STATUS_JOINING;
+
+public class SnipersTableModel extends AbstractTableModel {
+    private String statusText = STATUS_JOINING;
+    @Override
+    public int getRowCount() {
+        return 1;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 1;
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        return statusText;
+    }
+
+    public void setStatusText(String newStatusText) {
+        statusText = newStatusText;
+        fireTableRowsUpdated(0, 0);
+    }
+}
--- goos-java/src/test/java/auctionsniper/endtoend/AuctionSniperDriver.java
+++ goos-java/src/test/java/auctionsniper/endtoend/AuctionSniperDriver.java
@@ -4,11 +4,12 @@ import auctionsniper.MainWindow;
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.ComponentSelector;
 import com.objogate.wl.swing.driver.JFrameDriver;
-import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 import javax.swing.*;
 
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 @SuppressWarnings("unchecked")
@@ -24,7 +25,6 @@ public class AuctionSniperDriver extends JFrameDriver {
     }
 
     public void showsSniperStatus(String statusText) {
-        new JLabelDriver(this, named(MainWindow.SNIPER_STATUS_NAME))
-                .hasText(equalTo(statusText));
+        new JTableDriver(this).hasCell(withLabelText(equalTo(statusText)));
     }
 }
